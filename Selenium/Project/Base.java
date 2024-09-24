51ec9f2e5926b0ae80a1c69454a0c6b24fd89908
MandaKameshwari <Manda.VLB.Kameshwari@ibm.com>
2024-09-24 16:44:26 +0530

    Update Base.java

--- Selenium/Project/Base.java
+++ Selenium/Project/Base.java
@@ -6,4 +6,24 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Base {
     WebDriver driver;
     WebDriverWait wait;
+
+    @BeforeClass
+    public void beforeMethod() {
+        // Set up the Firefox driver
+        WebDriverManager.firefoxdriver().setup();
+        //Create a new instance of the Firefox driver
+        driver = new FirefoxDriver();
+        
+        //Open browser
+        driver.get("http://alchemy.hguy.co/crm");
+      //define implicit wait
+    	driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
+    }
+    
+    @AfterClass
+    public void afterMethod() {
+        //Close the browser
+        driver.quit();
+    }
+    
 }
